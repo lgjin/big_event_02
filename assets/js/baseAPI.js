ab815a82aa15b70a401eee8008c4292d33a9419f
jin <1941795025@qq.com>
2021-03-14 10:50:55 +0800

    修改了文件

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,6 @@ $(function () {
     $.ajaxPrefilter(function (option) {
         console.log(option.url)
         let baseurl = 'http://api-breakingnews-web.itheima.net';
-        option.ulr = baseurl + option.url
+        option.url = baseurl + option.url
     })
 })
