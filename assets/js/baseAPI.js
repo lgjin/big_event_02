7747025b6d77e07d547cbff417a958f4c38adc5e
jin <1941795025@qq.com>
2021-03-15 19:47:48 +0800

    完成了首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,18 @@ $(function () {
         console.log(option.url)
         let baseurl = 'http://api-breakingnews-web.itheima.net';
         option.url = baseurl + option.url
+        option.complete = function (res) {
+            console.log(res)
+            let holdUp = res.responseJSON
+            console.log(holdUp)
+            //判断
+            if (holdUp.status != 0 && holdUp.message == "身份认证失败！") {
+                //删除用户身份认证信息
+                localStorage.removeItem('token');
+                //返回登录界面
+                location.href = '/login.html'
+            }
+
+        }
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+$(function () {
+    //获取用户信息,多次用到封装函数
+    getUserInfo()
+    //退出登录
+    $('.layui-nav-item').on('click', function () {
+        //eg1
+        layer.confirm('确认退出?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            //删除用户身份认证信息
+            localStorage.removeItem('token');
+            //返回登录界面
+            location.href = '/login.html'
+            layer.close(index);
+        });
+    })
+
+});
+function getUserInfo() {
+    $.ajax({
+        url: '/my/userinfo',
+
+        headers: {
+            Authorization: localStorage.getItem('token') || ''
+        },
+        success: (res) => {
+            console.log(res);
+            if (res.status != 0) {
+                return layui.layer.msg(res.message, { icon: 5 });
+            }
+            //更换头像
+            change(res.data)
+
+        }
+    })
+};
+function change(item) {
+    let name = item.nickname || item.username
+    $('.welcome').html('欢迎&nbsp;&nbsp;' + name);
+    //判断头像是否为空
+    if (item.user_pic == null) {
+        $('.layui-nav-img').hide();
+
+        $('.text-avatar').show().html(name[0].toUpperCase())
+
+    }
+    else {
+        $('.layui-nav-img').show().attr('src', item.user_pic);
+        $('.text-avatar').hide()
+    }
+}
